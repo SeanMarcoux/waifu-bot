7b9399975a6b542c2e756345e7a58dce4bf40024
SeanMarcoux <seanmarcoux@hotmail.com>
2018-03-03 04:00:19 -0500

    You can now cast votes for best girl

--- bot.js
+++ bot.js
@@ -3,12 +3,16 @@ const client = new Discord.Client();
 const fs = require('fs');
 const request = require('request');
 
+var waifuDir = "waifus";
 var currentWaifu = "misc";
+var waifuScores = []
 
 client.on('ready', () => {
-    if(!fs.existsSync("waifus")) {
-        fs.mkdirSync("waifus");
+    if(!fs.existsSync(waifuDir)) {
+        fs.mkdirSync(waifuDir);
     }
+    initializeWaifuScores();
+    
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setActivity("Loving all my waifus");
     var channels = client.channels.array();
@@ -19,9 +23,11 @@ client.on('ready', () => {
     }
 });
 
-function sendDm(dmChannel, message)
-{
-    dmChannel.send(message);
+function initializeWaifuScores() {
+    var files = fs.readdirSync(waifuDir);
+    for(var i = 0; i < files.length; i++) {
+        waifuScores[files[i]] = 0;
+    }
 }
 
 client.on('message', msg => {
@@ -64,6 +70,12 @@ function reactToBS(msg, message)
     else if(message.startsWith("!current")) {
         msg.reply("I'm currently waiting for pictures of " + currentWaifu);
     }
+    else if(message.startsWith("!bestgirl ")) {
+        voteForBestGirl(msg);
+    }
+    else if(message.startsWith("!whoisbest")) {
+        whoIsBest(msg);
+    }
     else {
         msg.reply("I didn't understand that command. If it was meant for another bot, my bad!");
     }
@@ -76,25 +88,27 @@ function help(msg) {
         + "*!waifu (name of waifu)*: I'll show you a picture of the waifu you specified\n"
         + "*!list*: I'll tell you all of the waifus I currently have pictures of\n"
         + "*!incoming (name of waifu)*: I prepare to receive a picture of the waifu with that name. If I receive any pictures, I will save it under the folder for that waifu\n"
-        + "*!current*: I'll tell you the name of the waifu I'm currently waiting for");
+        + "*!current*: I'll tell you the name of the waifu I'm currently waiting for\n"
+        + "*!bestgirl (name of waifu)*: This will cast a vote for the specified girl as Best Girl\n"
+        + "*!whoisbest*: I'll give you the current ranking of the waifus based on your votes");
 }
 
 function noWaifuNoLaifu(msg) {
     var chosenWaifu = getStringAfterSpace(msg.content.toLowerCase());
     if(chosenWaifu) {
-        if(!fs.existsSync("waifus/" + chosenWaifu)) {
+        if(!fs.existsSync(waifuDir + "/" + chosenWaifu)) {
             msg.reply("I have no pictures of " + chosenWaifu + " D:\n"
                     + "I am now awaiting pictures of that waifu, so feel free to send me some now!");
             currentWaifu = chosenWaifu;
             return;
         }
-        var waifuPic = getRandomFileFromFolder("waifus/"+chosenWaifu);
+        var waifuPic = getRandomFileFromFolder(waifuDir+"/"+chosenWaifu);
     }
     else {
-        chosenWaifu = getRandomFileFromFolder("waifus");
-        var waifuPic = getRandomFileFromFolder("waifus/"+chosenWaifu);
+        chosenWaifu = getRandomFileFromFolder(waifuDir);
+        var waifuPic = getRandomFileFromFolder(waifuDir+"/"+chosenWaifu);
     }
-    msg.channel.send("", new Discord.Attachment("waifus/"+chosenWaifu+"/"+waifuPic));
+    msg.channel.send("", new Discord.Attachment(waifuDir+"/"+chosenWaifu+"/"+waifuPic));
 }
 
 function getStringAfterSpace(string) {
@@ -113,7 +127,7 @@ function getRandomInt(min, max) {
 }
 
 function listWaifus(msg) {
-    var files = fs.readdirSync("waifus");
+    var files = fs.readdirSync(waifuDir);
     var waifuList = "Here are all of the waifus I currently have pictures of:\n";
     for(var i = 0; i < files.length; i++) {
         waifuList += (i+1) + ". " + files[i] + "\n"
@@ -133,27 +147,66 @@ function receivePicture(msg) {
     for(var i=0; i < attachments.length; i++) {
         console.log("Downloading file " + attachments[i].filename);
         var filename = attachments[i].filename;
-        if(!fs.existsSync("waifus/" + currentWaifu))
-            fs.mkdirSync("waifus/" + currentWaifu);
-        while(fs.existsSync("waifus/" + currentWaifu + "/" + filename)) {
+        if(!fs.existsSync(waifuDir+"/" + currentWaifu)) {
+            fs.mkdirSync(waifuDir+"/" + currentWaifu);
+            waifuScores[currentWaifu] = 0;
+        }
+        while(fs.existsSync(waifuDir+"/" + currentWaifu + "/" + filename)) {
             filename_count++;
             split_name = filename.split(".");
             filename = filename_count + "." + split_name[split_name.length-1];
         }
-        console.log("Downloading it as " + "waifus/" + currentWaifu + "/" + filename);
-        download(attachments[i].url, "waifus/"+currentWaifu+"/"+filename, function() {
+        console.log("Downloading it as " + waifuDir + "/" + currentWaifu + "/" + filename);
+        download(attachments[i].url, waifuDir+"/"+currentWaifu+"/"+filename, function() {
             console.log("Done");
         });
     }
 }
 
-function download(uri, filename, callback){
-  request.head(uri, function(err, res, body){
+function download(uri, filename, callback) {
+  request.head(uri, function(err, res, body) {
     console.log('content-type:', res.headers['content-type']);
     console.log('content-length:', res.headers['content-length']);
 
     request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
   });
+}
+
+function voteForBestGirl(msg) {
+    var message = msg.content.toLowerCase();
+    var bestGirl = getStringAfterSpace(message);
+    
+    if(!fs.existsSync(waifuDir + "/" + bestGirl)) {
+        msg.reply("I have no pictures of " + bestGirl + " D:\n"
+                + "I am now awaiting pictures of that waifu, so feel free to send me some now!");
+        currentWaifu = bestGirl;
+        return;
+    }
+    
+    waifuScores[bestGirl]++;
+    msg.reply("Vote cast for " + bestGirl + ". They are now at " + waifuScores[bestGirl] + " points!");
+}
+
+function whoIsBest(msg) {
+    var bestGirls = getRankedGirlList();
+    
+    var girlRankings = "Best girls are:\n";
+    for(var i = 0; i < bestGirls.length; i++) {
+        girlRankings += (i+1) + ". " + bestGirls[i][0] + ": " + bestGirls[i][1] + "\n";
+    }
+    msg.channel.send(girlRankings);
+}
+
+function getRankedGirlList() {
+    var bestGirls = Object.keys(waifuScores).map(function(key) {
+        return [key, waifuScores[key]];
+    });
+
+    // Sort the array based on the second element
+    bestGirls.sort(function(first, second) {
+        return second[1] - first[1];
+    });
+    return bestGirls;
 }
 
 
